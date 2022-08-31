36030645db6f1f9d2c7aa485a200752940ae9210
unknown <syunyasisotto@gmail.com>
2022-08-31 16:12:09 +0900

    チャットの送信のみ完成

--- lib/view/Chat.dart
+++ lib/view/Chat.dart
@@ -1,23 +1,12 @@
-import 'dart:convert';
-import 'dart:io';
+// import 'dart:convert';
+// import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
+// import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
-import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:http/http.dart' as http;
-import 'package:image_picker/image_picker.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:path_provider/path_provider.dart';
 import "package:cloud_firestore/cloud_firestore.dart";
 import 'package:firebase_auth/firebase_auth.dart';
-import "package:flutter/cupertino.dart";
-import "package:flutter/material.dart";
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:projectritsbook_native/view_model/FetchBook.dart';
-import "package:projectritsbook_native/view_model/Items.dart";
-import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
-import 'package:projectritsbook_native/view/EditItem.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+
 
 
 class ChatPage extends StatefulWidget {
@@ -30,40 +19,69 @@ class ChatPage extends StatefulWidget {
 
 class _ChatPageState extends State<ChatPage> {
   List<types.Message> _messages = [];
-  final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
+  String message = '';
+  final String uid = FirebaseAuth.instance.currentUser!.uid; 
+  final _user = const types.User(
+    id: 'uid', firstName: '名前');
+
+  // final user = const types.User(
+  //   id: FirebaseAuth.instance.currentUser!.uid,
+  //   name: FirebaseAuth.instance.currentUser!.displayName,
+  // ); 
   @override
   void initState() {
     super.initState();
-    _loadMessages();
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: Chat(
-          messages: _messages,
-          // onAttachmentPressed: _handleAtachmentPressed,
-          onMessageTap: _handleMessageTap,
-          onPreviewDataFetched: _handlePreviewDataFetched,
-          onSendPressed: _handleSendPressed,
-          showUserAvatars: true,
-          showUserNames: true,
-          user: _user,
+        appBar: AppBar(
+          title: Text('チャット'),
+        ),
+        body: Column(
+          mainAxisAlignment:MainAxisAlignment.end,
+          children: <Widget>[
+            // builder:(context,spnapshot)=>Chat(
+            //   messages: _messages,
+            //   onSendPressed: _sendMessage,
+            //   user: _user,
+            // ),
+            // Chat(
+            //   messages: _messages,
+            //   onSendPressed: _sendMessage,
+            //   user: _user,
+            //   ),
+            TextField(
+              onChanged: (value) => message = value,
+              decoration: InputDecoration(
+                hintText: 'メッセージを入力してください',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: (){
+              _addMessage(message); 
+              },
+            child: Text('送信')),
+          ],
         ),
       );
 
-  void _addMessage(types.Message message) async{
+  void _sendMessage(types.PartialText text) {
+    print(text);
+  }
+  void _addMessage(String message) async{
     print('aaaaaaaaaaaaaaaaaaaaaaaa');
-    setState(() {
-      _messages.insert(0, message);
-    });
     await FirebaseFirestore.instance.collection('textbooks').doc(widget.document.id).collection('chat').doc().set({
       'uid': FirebaseAuth.instance.currentUser!.uid,
       'message': message,
       'created': DateTime.now(),
       'userName' : FirebaseAuth.instance.currentUser!.displayName,
     }).then((value) => print("success")).catchError((error) => print(error));
+    message='';
   }
-
+  void _handleSendPressed(types.PartialText message) async {
+    _addMessage(message.text);
+  }
   void _getMessages()async {
     final getData = await FirebaseFirestore.instance.collection('textbooks').doc(widget.document.id).collection('chat').get();
     final messages = getData.docs.
@@ -73,81 +91,4 @@ class _ChatPageState extends State<ChatPage> {
     });
   }
 
-  void _handleMessageTap(BuildContext _, types.Message message) async {
-    if (message is types.FileMessage) {
-      var localPath = message.uri;
-
-      if (message.uri.startsWith('http')) {
-        try {
-          final index =
-              _messages.indexWhere((element) => element.id == message.id);
-          final updatedMessage =
-              (_messages[index] as types.FileMessage).copyWith(
-            isLoading: true,
-          );
-
-          setState(() {
-            _messages[index] = updatedMessage;
-          });
-
-          final client = http.Client();
-          final request = await client.get(Uri.parse(message.uri));
-          final bytes = request.bodyBytes;
-          final documentsDir = (await getApplicationDocumentsDirectory()).path;
-          localPath = '$documentsDir/${message.name}';
-
-          if (!File(localPath).existsSync()) {
-            final file = File(localPath);
-            await file.writeAsBytes(bytes);
-          }
-        } finally {
-          final index =
-              _messages.indexWhere((element) => element.id == message.id);
-          final updatedMessage =
-              (_messages[index] as types.FileMessage).copyWith(
-            isLoading: null,
-          );
-
-          setState(() {
-            _messages[index] = updatedMessage;
-          });
-        }
-      }
-    }
-  }
-
-  void _handlePreviewDataFetched(
-    types.TextMessage message,
-    types.PreviewData previewData,
-  ) {
-    final index = _messages.indexWhere((element) => element.id == message.id);
-    final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
-      previewData: previewData,
-    );
-
-    setState(() {
-      _messages[index] = updatedMessage;
-    });
-  }
-
-  void _handleSendPressed(types.PartialText message) {
-    final textMessage = types.TextMessage(
-      author: _user,
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      id: 'uuuuuuu',
-      text: message.text,
-    );
-    _addMessage(textMessage);
-  }
-
-  void _loadMessages() async {
-    final response = await rootBundle.loadString('assets/messages.json');
-    final messages = (jsonDecode(response) as List)
-        .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
-        .toList();
-
-    setState(() {
-      _messages = messages;
-    });
-  }
 }
