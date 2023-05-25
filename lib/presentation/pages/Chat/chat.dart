01a5e7b16ee08576693efe4a0938dced9d98d973
sisosyunya <syunyasisotto@gmail.com>
2023-05-25 23:34:58 +0900

    [update]chat

--- lib/presentation/pages/Chat/chat.dart
+++ lib/presentation/pages/Chat/chat.dart
@@ -1,122 +1,40 @@
 import 'package:flutter/material.dart';
 import "package:cloud_firestore/cloud_firestore.dart";
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
-import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
-import 'package:http/http.dart' as http;
-import 'package:image_picker/image_picker.dart';
-import 'package:mime/mime.dart';
-import 'package:open_filex/open_filex.dart';
-import 'package:path_provider/path_provider.dart';
 
-class ChatPage extends StatefulWidget {
+class ChatPage extends StatelessWidget {
   final String documentId;
   ChatPage(this.documentId);
-  @override
-  State<ChatPage> createState() => _ChatPageState();
-}
-
-class _ChatPageState extends State<ChatPage> {
-  List<types.Message> _messages = [];
-  String randomId = DateTime.now().millisecondsSinceEpoch.toString();
-  // final username = FirebaseAuth.instance.currentUser!.displayName;
-  final _user = types.User(
-    id: '313wfww',
-    // firstName: 'test',
-  );
-  @override
-  void initState() {
-    _getMessages();
-    super.initState();
-  }
-
-  void _getMessages() async {
-    final getData = await FirebaseFirestore.instance
-        .collection("textbooks")
-        .doc(widget.documentId)
-        .collection("chat")
-        .get();
-
-    final message = getData.docs
-        .map((d) => types.TextMessage(
-              author: types.User(
-                id: d["user"]["id"],
-                firstName: d["user"]["firstName"],
-              ),
-              id: d.id,
-              text: d.data()["text"],
-            ))
-        .toList();
-    setState(() {
-      _messages = message;
-    });
-  }
-
-  // メッセージ内容をfirestoreにセット
-  void _addMessage(types.Message message) async {
-    setState(() {
-      _messages.insert(0, message);
-    });
-    await FirebaseFirestore.instance
-        .collection("textbooks")
-        .doc(widget.documentId)
-        .collection("chat")
-        .doc()
-        .set({
-          "createdAt": message.createdAt,
-          "id": message.id,
-          "text": message,
-          "user": {
-            "id": message.author.id,
-            "firstName": message.author.firstName,
-          }
-        })
-        .then((value) => print("success"))
-        .catchError((error) {
-        });
-    // 通知処理
-    // if (FirebaseAuth.instance.currentUser!.uid != widget.['userID']) {
-    //   await FirebaseFirestore.instance
-    //       .collection('users')
-    //       .doc(widget.document["userID"])
-    //       .collection('information')
-    //       .doc(widget.document.id)
-    //       .set({
-    //         'information': "出品中の商品「”${widget.document["item"]}”」にメッセージを送信しました。",
-    //         'isRead': false,
-    //         'timestamp': DateTime.now(),
-    //       })
-    //       .then((value) => print("success"))
-    //       .catchError((error) => print(error));
-    // }
-  }
-
-  void _handleSendPressed(types.PartialText message) {
-    final newMessage = types.TextMessage(
-      author: _user,
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      id: randomId,
-      text: message.text,
-    );
-    _addMessage(newMessage);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("チャット"),
+        title: const Text('チャット'),
+        // backgroundColor: const Color(0xffff6b6b),
+        elevation: 0,
       ),
-      body: Chat(
-        messages: _messages,
-        onSendPressed: _handleSendPressed,
-        user: _user,
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('textbooks')
+            .doc(documentId)
+            .collection('chat')
+            .orderBy('createdAt', descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final List<DocumentSnapshot> documents = snapshot.data!.docs;
+            return ListView(
+              children: documents.map((document) {
+                return Card(
+                  child: ListTile(
+                    title: Text(document['text']),
+                    subtitle: Text(document['createdAt'].toString()),
+                  ),
+                );
+              }).toList(),
+            );
+          }
+          return const Text('読み込み中...');
+        },
       ),
     );
   }
