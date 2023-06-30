9e5d34c17a0bc4817579abe87cd290c75925182d
sisosyunya <syunyasisotto@gmail.com>
2023-06-30 14:51:29 +0900

    add min testcode

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,23 +1,39 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-
-// import 'package:projectritsbook_native/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:projectritsbook_native/main.dart';
+import 'package:projectritsbook_native/presentation/pages/exhibition.dart';
+import 'package:projectritsbook_native/presentation/pages/landing_page/landing_page.dart';
+import 'package:projectritsbook_native/presentation/pages/my_page/my_page.dart';
+import 'package:projectritsbook_native/presentation/pages/notification.dart';
 
 void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(const MyApp());
+  testWidgets('RitsBook navigates correctly', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const ProviderScope(child: RitsBook()));
 
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
+    // Verify that RitsBook starts with LandingPage.
+    expect(find.byType(LandingPage), findsOneWidget);
 
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
+    // Tap the 'お知らせ' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.notifications));
+    await tester.pumpAndSettle();
 
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
+    // Verify that tapping 'お知らせ' icon navigates to NotificationPage.
+    expect(find.byType(NotificationPage), findsOneWidget);
+
+    // Tap the '出品' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.camera_alt_outlined));
+    await tester.pumpAndSettle();
+
+    // Verify that tapping '出品' icon navigates to ExhibitionPage.
+    expect(find.byType(ExhibitionPage), findsOneWidget);
+
+    // Tap the 'マイページ' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.person));
+    await tester.pumpAndSettle();
+
+    // Verify that tapping 'マイページ' icon navigates to MyPage.
+    expect(find.byType(MyPage), findsOneWidget);
+  });
 }
