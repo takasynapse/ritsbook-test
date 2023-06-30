672311c152150d202eb3710b21feb921cc9fd236
sisosyunya <syunyasisotto@gmail.com>
2023-06-30 14:46:13 +0900

    fix warnings2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,7 @@
 
 // import 'package:projectritsbook_native/main.dart';
 
-// void main() {
+void main() {
 //   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
 //     // Build our app and trigger a frame.
 //     await tester.pumpWidget(const MyApp());
@@ -20,4 +20,4 @@
 //     expect(find.text('0'), findsNothing);
 //     expect(find.text('1'), findsOneWidget);
 //   });
-// }
+}
